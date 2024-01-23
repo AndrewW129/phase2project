01220dcec12480c92abd113c9c8573f23896cbee
Andrew Wilson <adwilson12901@gmail.com>
2024-01-23 15:45:56 -0500

    more components

---
+++ src/AddQForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddQForm() {
+  return (
+    <div>AddQForm</div>
+  )
+}
+
+export default AddQForm
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import QuestionList from './QuestionList'
+import Leaderboard from './Leaderboard'
+import AddQForm from './AddQForm'
 
 function Header() {
+
+  const handleClick = (e) => e.target.textContent === 'Light Mode'? e.target.textContent = 'Dark Mode' : e.target.textContent = 'Light Mode'
+
   return (
-    <div>Header</div>
+
+    <div>Header
+      <img src="" alt="Logo" />
+      <p>USER'S NAME</p>
+      <button onClick={handleClick}name="toggle">Light Mode</button>
+      <QuestionList />
+      <Leaderboard />
+      <AddQForm />
+    </div>
   )
 }
 
