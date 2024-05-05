c268cd9c54d98c598a0a25bc7c8d2e4983d5294a
Andrew Wilson <adwilson12901@gmail.com>
2024-05-05 16:57:47 -0400

    made more user-friendly

--- src/QuestionList.js
+++ src/QuestionList.js
@@ -4,21 +4,9 @@ import QuestionItem from './QuestionItem'
 function QuestionList({questions, setQuestions, onScore, points}) {
 
   useEffect(() => {
-    let isMounted = true;
-  
     fetch('https://the-trivia-api.com/v2/questions?limit=1')
     .then(response => response.json())
-    .then(data => {
-        if (isMounted) {
-          // Update state only if the component is still mounted
-          setQuestions(data);
-        }
-      })
-  
-    return () => {
-      // Cleanup: set isMounted to false when component unmounts
-      isMounted = false;
-    };
+    .then(data => setQuestions(data))
 }, []);
   
 
